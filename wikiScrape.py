b9737e39d9f9caf12a166ab7741acd2c05f8c34b
andrewxding <andrewxding@yahoo.com>
2017-09-11 21:26:16 -0700

    Update wikiScrape.py

--- wikiScrape.py
+++ wikiScrape.py
@@ -5,43 +5,46 @@ from bs4 import BeautifulSoup
 import wikipedia
 from urllib.request import urlopen
 
-# # baseurl = 'http://en.wikipedia.org/w/api.php'
-# # my_atts = {
-# # 	"action": "opensearch",
-# # 	"format": "json",
-# # 	"search": "Ed Sheeran",
-# # 	"namespace": "0",
-# # 	"limit": "10",
-# # 	"utf8": True,
-# # 	"formatversion" : 2
-# # }
-# # r = requests.get(baseurl, my_atts)
-# # print(r.status_code)
-# # #soup = BeautifulSoup(r.content, 'html.parser')
-
-# import wikipedia
-# print (wikipedia.summary("Wikipedia"))
-
-# baseurl = 'http://en.wikipedia.org/w/api.php'
-# my_atts = {
-# 	"action": "query",
-# 	"format": "json",
-# 	"titles": "Ed Sheeran",
-# 	"prop" : "info"
-# }
+def getPage(name):
+	baseurl = 'http://en.wikipedia.org/w/api.php'
+	my_atts = {
+		"action": "opensearch",
+		"format": "json",
+		"search": name,
+		"namespace": "0",
+		"limit": "10",
+		"utf8": True,
+		"formatversion" : 2
+	}
+	r = requests.get(baseurl, my_atts)
+	print(r.status_code)
+	#soup = BeautifulSoup(r.content, 'html.parser')
 
 
-# header = {'User-Agent': 'Mozilla/5.0'} #Needed to prevent 403 error on Wikipedia
-# r = requests.get(baseurl, headers=header, params = my_atts)
-# r = r.json()
-# print(r["query"]["pages"].keys()[0])
+def getSummary(name):
+	print(wikipedia.summary(name))
+
+	baseurl = 'http://en.wikipedia.org/w/api.php'
+	my_atts = {
+		"action": "query",
+		"format": "json",
+		"titles": name,
+		"prop" : "info"
+	}
+
+
+	header = {'User-Agent': 'Mozilla/5.0'} #Needed to prevent 403 error on Wikipedia
+	r = requests.get(baseurl, headers=header, params = my_atts)
+	r = r.json()
+	return(r["query"]["pages"].keys()[0])
 
 
 
 
 #f = wptools.page("Ed Sheeran").get_query()
-fela = wptools.page('Ed Sheeran').get_parse()
-for info in fela.infobox.keys():
-		if info != "image" and info != "labels":
-			print(fela.infobox[info])
-#print(f.extext)
+def getArtistInfo(name):
+	fela = wptools.page(name).get_parse()
+	for info in fela.infobox.keys():
+			if info != "image" and info != "labels":
+				return(fela.infobox[info])
+
